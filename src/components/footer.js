ac8c07040ff7d61e81f7d5c18f026d08b9a81367
Alexander Aspman <aspmanalexander@gmail.com>
2021-05-18 18:47:05 +0200

    footer icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,6 @@
 import github from '../images/github.svg' 
 import linkedin from '../images/linkedin.svg' 
-import phone from '../images/telephone.svg' 
-import envelope from '../images/envelope.svg' 
- 
+
  
  const Footer = () => {
 
@@ -16,15 +14,15 @@ return(
         <div className="row footer">
 
 
-            <div className="footer__contact col-lg-7">
+            <div className="footer__contact col-md-7">
              <p>I am currently looking for job as a developer. If you find my work interesting and want to find out more about me, contact me at: aspmanalexander@gmail.com.</p>
          
 
             </div>
          
-            <div className="col-5">
+            <div className="col-md-5">
             <div className="row footer__contact ">
-            <div                className="col-lg-2 col-4"
+            <div                className="col-2 "
 >
                
                <img 
@@ -33,12 +31,12 @@ return(
               
     
     </div> 
-    <div className="col-lg-10 col-8">   <a href="github.com/alexanderaspman">github.com/alexanderaspman</a></div>
+    <div className="col-10 ">   <a href="github.com/alexanderaspman">github.com/alexanderaspman</a></div>
   
     </div> 
     
      <div className="row footer__contact">
-     <div                className="col-lg-2 col-4"
+     <div                className="col-2"
 >
                
                <img 
@@ -46,7 +44,7 @@ return(
                alt=''/>
     
     </div>
-    <div className="col-lg-10 col-8"> <a href="linkedin.com/in/alexanderaspman">linkedin.com/in/alexanderaspman</a></div>
+    <div className="col-10"> <a href="linkedin.com/in/alexanderaspman">linkedin.com/in/alexanderaspman</a></div>
    
     </div>
           
